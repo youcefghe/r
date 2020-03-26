f52096019459a07152165bf4febeeb7aaecc6317
CBY <bvg>
2020-03-26 22:40:09 +0100

    adding middlewares with morgan

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+//using morgan for middleware
+if (process.env.NODE_ENV == 'developement') {
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000; // process.env its a default format for the envirment
